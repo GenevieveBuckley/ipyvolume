7e4c08af6b9e5562f364d8da2e8e7f3a2f9a22dc
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-05-15 23:14:25 +1000

    black format ipyvollume/styles.py

--- ipyvolume/styles.py
+++ ipyvolume/styles.py
@@ -31,8 +31,12 @@ from ipyvolume import utils
 
 styles = {}
 _defaults = {
-    'axes': {'visible': True, 'label': {'color': 'black'}, 'ticklabel': {'color': 'black'}},
-    'box': {'visible': True},
+    "axes": {
+        "visible": True,
+        "label": {"color": "black"},
+        "ticklabel": {"color": "black"},
+    },
+    "box": {"visible": True},
 }
 
 
@@ -41,7 +45,7 @@ def create(name, properties):
     return properties
 
 
-light = create("light", {'background-color': 'white', 'axes': {'color': 'black'}})
+light = create("light", {"background-color": "white", "axes": {"color": "black"}})
 
 default = {}
 utils.dict_deep_update(default, _defaults)
@@ -50,34 +54,52 @@ utils.dict_deep_update(default, light)
 dark = create(
     "dark",
     {
-        'background-color': '#000001',  # for some reason we cannot set it to black!?!
-        'axes': {'color': 'white', 'label': {'color': 'white'}, 'ticklabel': {'color': 'white'}},
+        "background-color": "#000001",  # for some reason we cannot set it to black!?!
+        "axes": {
+            "color": "white",
+            "label": {"color": "white"},
+            "ticklabel": {"color": "white"},
+        },
     },
 )
 
 demo = create(
     "demo",
     {
-        'background-color': 'white',
-        'box': {'color': 'pink', 'visible': True},
-        'axes': {
-            'color': 'black',
-            'visible': True,
-            'x': {'color': '#f00', 'label': {'color': '#0f0'}, 'ticklabel': {'color': '#00f'}},
-            'y': {'color': '#0f0', 'label': {'color': '#00f'}, 'ticklabel': {'color': '#f00'}},
-            'z': {'color': '#00f', 'label': {'color': '#f00'}, 'ticklabel': {'color': '#0f0'}},
+        "background-color": "white",
+        "box": {"color": "pink", "visible": True},
+        "axes": {
+            "color": "black",
+            "visible": True,
+            "x": {
+                "color": "#f00",
+                "label": {"color": "#0f0"},
+                "ticklabel": {"color": "#00f"},
+            },
+            "y": {
+                "color": "#0f0",
+                "label": {"color": "#00f"},
+                "ticklabel": {"color": "#f00"},
+            },
+            "z": {
+                "color": "#00f",
+                "label": {"color": "#f00"},
+                "ticklabel": {"color": "#0f0"},
+            },
         },
     },
 )
 
-minimal = {'box': {'visible': False}, 'axes': {'visible': False}}
-nobox = create("nobox", {'box': {'visible': False}, 'axes': {'visible': True}})
+minimal = {"box": {"visible": False}, "axes": {"visible": False}}
+nobox = create("nobox", {"box": {"visible": False}, "axes": {"visible": True}})
 
 if __name__ == "__main__":
     source = __file__
     dest = os.path.join(os.path.dirname(source), "../js/data/style.json")
     print(source, dest)
-    need_update = (not os.path.exists(dest)) or (os.path.getmtime(source) > os.path.getmtime(dest))
+    need_update = (not os.path.exists(dest)) or (
+        os.path.getmtime(source) > os.path.getmtime(dest)
+    )
     if need_update:
         print(styles)
         with open(dest, "w") as f:
