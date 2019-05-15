829f3bc428e50b0e79e6698b8892bcea8cf45bbd
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-05-15 23:16:53 +1000

    black format setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,20 @@ import platform
 from distutils import log
 
 here = os.path.dirname(os.path.abspath(__file__))
-node_root = os.path.join(here, 'js')
-is_repo = os.path.exists(os.path.join(here, '.git'))
+node_root = os.path.join(here, "js")
+is_repo = os.path.exists(os.path.join(here, ".git"))
 
-npm_path = os.pathsep.join([os.path.join(node_root, 'node_modules', '.bin'), os.environ.get('PATH', os.defpath)])
+npm_path = os.pathsep.join(
+    [
+        os.path.join(node_root, "node_modules", ".bin"),
+        os.environ.get("PATH", os.defpath),
+    ]
+)
 
 
 log.set_verbosity(log.DEBUG)
-log.info('setup.py entered')
-log.info('$PATH=%s' % os.environ['PATH'])
+log.info("setup.py entered")
+log.info("$PATH=%s" % os.environ["PATH"])
 
 
 def read(fname):
@@ -30,25 +35,26 @@ LONG_DESCRIPTION = read("README.rst")
 
 def js_prerelease(command, strict=False):
     """Decorator for building minified js/css prior to another command."""
+
     class DecoratedCommand(command):
         def run(self):
-            jsdeps = self.distribution.get_command_obj('jsdeps')
+            jsdeps = self.distribution.get_command_obj("jsdeps")
             if not is_repo and all(os.path.exists(t) for t in jsdeps.targets):
                 # sdist, nothing to do
                 command.run(self)
                 return
 
             try:
-                self.distribution.run_command('jsdeps')
+                self.distribution.run_command("jsdeps")
             except Exception as e:
                 missing = [t for t in jsdeps.targets if not os.path.exists(t)]
                 if strict or missing:
-                    log.warn('rebuilding js and css failed')
+                    log.warn("rebuilding js and css failed")
                     if missing:
-                        log.error('missing files: %s' % missing)
+                        log.error("missing files: %s" % missing)
                     raise e
                 else:
-                    log.warn('rebuilding js and css failed (not a problem)')
+                    log.warn("rebuilding js and css failed (not a problem)")
                     log.warn(str(e))
             command.run(self)
             update_package_data(self.distribution)
@@ -58,23 +64,23 @@ def js_prerelease(command, strict=False):
 
 def update_package_data(distribution):
     """Update package_data to catch changes during setup."""
-    build_py = distribution.get_command_obj('build_py')
+    build_py = distribution.get_command_obj("build_py")
     # distribution.package_data = find_package_data()
     # re-init build_py options which load package_data
     build_py.finalize_options()
 
 
 class NPM(Command):
-    description = 'install package.json dependencies using npm'
+    description = "install package.json dependencies using npm"
 
     user_options = []
 
-    node_modules = os.path.join(node_root, 'node_modules')
+    node_modules = os.path.join(node_root, "node_modules")
 
     targets = [
-        os.path.join(here, 'ipyvolume', 'static', 'extension.js'),
-        os.path.join(here, 'ipyvolume', 'static', 'index.js'),
-        os.path.join(here, 'ipyvolume', 'static', 'three.js'),
+        os.path.join(here, "ipyvolume", "static", "extension.js"),
+        os.path.join(here, "ipyvolume", "static", "index.js"),
+        os.path.join(here, "ipyvolume", "static", "three.js"),
     ]
 
     def initialize_options(self):
@@ -84,15 +90,15 @@ class NPM(Command):
         pass
 
     def get_npm_name(self):
-        npmName = 'npm'
-        if platform.system() == 'Windows':
-            npmName = 'npm.cmd'
+        npmName = "npm"
+        if platform.system() == "Windows":
+            npmName = "npm.cmd"
         return npmName
 
     def has_npm(self):
         npmName = self.get_npm_name()
         try:
-            check_call([npmName, '--version'])
+            check_call([npmName, "--version"])
             return True
         except:
             return False
@@ -108,19 +114,26 @@ class NPM(Command):
             )
 
         env = os.environ.copy()
-        env['PATH'] = npm_path
+        env["PATH"] = npm_path
 
         if self.should_run_npm_install():
-            log.info("Installing build dependencies with npm.  This may take a while...")
+            log.info(
+                "Installing build dependencies with npm.  This may take a while..."
+            )
             npmName = self.get_npm_name()
-            check_call([npmName, 'install'], cwd=node_root, stdout=sys.stdout, stderr=sys.stderr)
+            check_call(
+                [npmName, "install"],
+                cwd=node_root,
+                stdout=sys.stdout,
+                stderr=sys.stderr,
+            )
             os.utime(self.node_modules, None)
 
         for t in self.targets:
             if not os.path.exists(t):
-                msg = 'Missing file: %s' % t
+                msg = "Missing file: %s" % t
                 if not has_npm:
-                    msg += '\nnpm is required to build a development version of widgetsnbextension'
+                    msg += "\nnpm is required to build a development version of widgetsnbextension"
                 raise ValueError(msg)
 
         # update package data in case this created new files
@@ -128,63 +141,63 @@ class NPM(Command):
 
 
 version_ns = {}
-with open(os.path.join(here, 'ipyvolume', '_version.py')) as f:
+with open(os.path.join(here, "ipyvolume", "_version.py")) as f:
     exec(f.read(), {}, version_ns)
 
 setup_args = {
-    'name': 'ipyvolume',
-    'version': version_ns['__version__'],
-    'description': 'IPython widget for rendering 3d volumes',
-    'long_description': LONG_DESCRIPTION,
-    'include_package_data': True,
-    'data_files': [
+    "name": "ipyvolume",
+    "version": version_ns["__version__"],
+    "description": "IPython widget for rendering 3d volumes",
+    "long_description": LONG_DESCRIPTION,
+    "include_package_data": True,
+    "data_files": [
         (
-            'share/jupyter/nbextensions/ipyvolume',
+            "share/jupyter/nbextensions/ipyvolume",
             [
-                'ipyvolume/static/extension.js',
-                'ipyvolume/static/index.js',
-                'ipyvolume/static/three.js',
-                'ipyvolume/static/index.js.map',
+                "ipyvolume/static/extension.js",
+                "ipyvolume/static/index.js",
+                "ipyvolume/static/three.js",
+                "ipyvolume/static/index.js.map",
             ],
         ),
-        ('etc/jupyter/nbconfig/notebook.d', ['ipyvolume.json']),
+        ("etc/jupyter/nbconfig/notebook.d", ["ipyvolume.json"]),
     ],
-    'install_requires': [
-        'ipywidgets>=7.0.0',
-        'numpy',
-        'traittypes',
-        'traitlets',
-        'Pillow',
-        'ipywebrtc',
-        'requests',
-        'pythreejs>=2.0.0'
+    "install_requires": [
+        "ipywidgets>=7.0.0",
+        "numpy",
+        "traittypes",
+        "traitlets",
+        "Pillow",
+        "ipywebrtc",
+        "requests",
+        "pythreejs>=2.0.0",
     ],
-    'license': 'MIT',
-    'packages': find_packages(),
-    'zip_safe': False,
-    'cmdclass': {
-        'build_py': js_prerelease(build_py),
-        'egg_info': js_prerelease(egg_info),
-        'sdist': js_prerelease(sdist, strict=True),
-        'jsdeps': NPM,
+    "license": "MIT",
+    "packages": find_packages(),
+    "zip_safe": False,
+    "cmdclass": {
+        "build_py": js_prerelease(build_py),
+        "egg_info": js_prerelease(egg_info),
+        "sdist": js_prerelease(sdist, strict=True),
+        "jsdeps": NPM,
     },
-    'author': 'Maarten A. Breddels',
-    'author_email': 'maartenbreddels@gmail.com',
-    'url': 'https://github.com/maartenbreddels/ipyvolume',
-    'keywords': ['ipython', 'jupyter', 'widgets', 'volume rendering'],
-    'classifiers': [
-        'Development Status :: 4 - Beta',
-        'Framework :: IPython',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Multimedia :: Graphics',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+    "author": "Maarten A. Breddels",
+    "author_email": "maartenbreddels@gmail.com",
+    "url": "https://github.com/maartenbreddels/ipyvolume",
+    "keywords": ["ipython", "jupyter", "widgets", "volume rendering"],
+    "classifiers": [
+        "Development Status :: 4 - Beta",
+        "Framework :: IPython",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Multimedia :: Graphics",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
 }
 
