e4debc7af2e2880e3b365db922ceca0f57cdb139
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-05-15 23:15:17 +1000

    black format ipyvolume/traittypes.py

--- ipyvolume/traittypes.py
+++ ipyvolume/traittypes.py
@@ -6,7 +6,7 @@ class Image(TraitType):
     """A trait for PIL images."""
 
     default_value = None
-    info_text = 'a PIL Image object'
+    info_text = "a PIL Image object"
 
     def validate(self, obj, value):
         if isinstance(value, PIL.Image.Image):
