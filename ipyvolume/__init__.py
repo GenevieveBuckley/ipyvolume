1dafc08f8227681e440f8906529872a87cdb4b9d
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-05-16 18:10:46 +1000

    Use explicit imports instead of import * in ipyvolume/__init__.py

--- ipyvolume/__init__.py
+++ ipyvolume/__init__.py
@@ -5,9 +5,56 @@ from ipyvolume import styles  # noqa: F401
 from ipyvolume import examples  # noqa: F401
 from ipyvolume import datasets  # noqa: F401
 from ipyvolume import embed  # noqa: F401
-from ipyvolume.widgets import *  # noqa: F401, F403
-from ipyvolume.transferfunction import *  # noqa: F401, F403
-from ipyvolume.pylab import *  # noqa: F401, F403
+from ipyvolume.widgets import (Mesh,
+                               Scatter,
+                               Volume,
+                               Figure,
+                               quickquiver,
+                               quickscatter,
+                               quickvolshow,
+                               )  # noqa: F401
+from ipyvolume.transferfunction import (TransferFunction,
+                                        TransferFunctionJsBumps,
+                                        TransferFunctionWidgetJs3,
+                                        TransferFunctionWidget3,
+                                        )  # noqa: F401
+from ipyvolume.pylab import (current,
+                             clear,
+                             controls_light,
+                             figure,
+                             gcf,
+                             xlim,
+                             ylim,
+                             zlim,
+                             xyzlim,
+                             squarelim,
+                             plot_trisurf,
+                             plot_surface,
+                             plot_wireframe,
+                             plot_mesh,
+                             plot,
+                             scatter,
+                             quiver,
+                             show,
+                             animate_glyphs,
+                             animation_control,
+                             gcc,
+                             transfer_function,
+                             plot_isosurface,
+                             volshow,
+                             save,
+                             movie,
+                             screenshot,
+                             savefig,
+                             xlabel,
+                             ylabel,
+                             zlabel,
+                             xyzlabel,
+                             view,
+                             style,
+                             plot_plane,
+                             selector_default,
+                             )  # noqa: F401
 
 
 def _jupyter_nbextension_paths():
